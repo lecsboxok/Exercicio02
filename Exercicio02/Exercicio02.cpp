07ea6d8a2c5b9eac1c4d72c35306334fa78c3c53
lecsboxok <133366992+lecsboxok@users.noreply.github.com>
2024-04-12 16:45:22 -0300

    Initial commit

---
+++ Exercicio02/Exercicio02.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+//1 – Uma empresa vende sorvetes no valor atacado, se o cliente comprar até 25 unidades, é cobrado R$1.50 por unidade, acima disso, é cobrado R$1.25.Faça um código que calcule quanto um determinado cliente irá pagar
+
+//int main() {
+//
+//	int cliente = 30;
+//
+//	if (cliente <= 25) {
+//
+//		float valor = cliente * 1.50;
+//		cout << "O valor a ser pago é R$";
+//		cout << valor;
+//	}
+//	else {
+//		float valor = cliente * 1.25;
+//		cout << "O valor a ser pago é R$";
+//		cout << valor;
+//	}
+//
+//	return 0;
+//}
+
+//2 – Declare uma variável com um número, se ele for maior que 10, faça o cálculo do número + 10 * 2. Senão faça número + 9 * 3 – 8
+
+//int main() {
+//
+//	int numero = 10;
+//
+//	if (numero > 10) {
+//
+//		int calculo = numero + 10 * 2;
+//		cout << "O cáculo é ";
+//		cout << calculo;
+//	}
+//	else {
+//		int calculo = numero + 9 * 3 - 8;
+//		cout << "O cáculo é ";
+//		cout << calculo;
+//	}
+//
+//
+//	return 0;
+//}
+
+//3 – Faça um código que verifique se um número é positivo ou negativo
+
+//int main() {
+//
+//	int numero = 0;
+//
+//	if (numero < 0) {
+//		cout << numero;
+//		cout << " Negativo";
+//	}
+//	else if(numero > 0) {
+//		cout << numero;
+//		cout << " Positivo";
+//	}
+//	else {
+//		cout << numero;
+//		cout << " Neutro";
+//	}
+//
+//
+//	return 0;
+//}
+
+//4 – Faça um codigo que tenha duas variáveis numéricas e mostre qual delas é a maior
+
+//int main() {
+//
+//	int numero1 = 3;
+//	int numero2 = 6;
+//
+//	if (numero1 > numero2) {
+//		cout << numero1;
+//		cout << " é maior que ";
+//		cout << numero2;
+//	}
+//	else if (numero2 > numero1) {
+//		cout << numero2;
+//		cout << " é maior que ";
+//		cout << numero1;
+//	}
+//	else {
+//		cout << numero1;
+//		cout << " e ";
+//		cout << numero2;
+//		cout << " são iguais";
+//	}
+//
+//	return 0;
+//}
+
+//5 – Um professor deseja um sistema para o cálculo de média dos seus alunos, para isso, declare duas variáveis para as notas, realize o cálculo da média e mostre, se a média for maior ou igual a 7 mostre “Aprovado”, senão mostre “Reprovado”
+
+//int main() {
+//
+//	float nota1 = 9.5;
+//	float nota2 = 5.5;
+//
+//	float media = (nota1 + nota2) / 2;
+//
+//	if (media >= 7) {
+//		cout << "Sua media foi ";
+//		cout << media;
+//		cout << " voce foi aprovado";
+//	}
+//	else {
+//		cout << "Sua media foi ";
+//		cout << media;
+//		cout << " voce foi reprovado";
+//	}
+//
+//	return 0;
+//}
